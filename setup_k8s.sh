42c2143613a97139433a4c9948ba573aea042ef2
Antonio Lain <antlai@cafjs.com>
2018-07-28 22:14:51 -0700

    Update setup

--- setup_k8s.sh
+++ setup_k8s.sh
@@ -11,10 +11,10 @@ then
 else
     echo "Missing token, creating..."
     docker pull gcr.io/cafjs-k8/root-rpitoken
-    docker run --rm -e ACCOUNTS_URL=https://root-accounts.uhdia.com -e MY_ID="$1" -e PASSWD="$2" -v /config:/config gcr.io/cafjs-k8/root-rpitoken
+    docker run --rm -e ACCOUNTS_URL=https://root-accounts.cafjs.com -e MY_ID="$1" -e PASSWD="$2" -v /config:/config gcr.io/cafjs-k8/root-rpitoken
 fi
 chmod 600 token
 popd #/config
 
 docker rm -f root-rpidaemon || true
-docker run -d --name=root-rpidaemon --restart=always -e IS_REGISTRY_PRIVATE=false -e APP_SUFFIX=uhdia.com -e MY_ID="$1" -v /var/run/docker.sock:/var/run/docker.sock  -v /config:/config  -e CONFIG_VOLUME=/config gcr.io/cafjs-k8/root-rpidaemon
+docker run -d --name=root-rpidaemon --restart=always -e IS_REGISTRY_PRIVATE=false -e APP_SUFFIX=cafjs.com -e MY_ID="$1" -v /var/run/docker.sock:/var/run/docker.sock  -v /config:/config  -e CONFIG_VOLUME=/config gcr.io/cafjs-k8/root-rpidaemon
